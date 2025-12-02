a6015cc00b588b48d9f053f933a80a0437602369
Mahamudun Nabi Siam <mahamudunn1005@gmail.com>
2025-12-02 07:10:46 +0000

    Try fixing this error: `Runtime Error: Error: Invalid src prop (http://i

--- next.config.ts
+++ next.config.ts
@@ -28,6 +28,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'http',
+        hostname: 'img-cdn.curl.pk',
+        port: '8880',
+        pathname: '/**',
+      },
     ],
   },
 };
